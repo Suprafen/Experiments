c125320e6f84792878ed541b8fe5b950aa331536
Ivan <ivan.skif41@gmail.com>
2022-06-17 10:35:15 +0300

    Initial commit

---
+++ UIKit/CustomDetentSheet/CustomDetentSheet/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CustomDetentSheet
+//
+//  Created by Ivan Pryhara on 17.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+
+    func configureView() {
+        self.view.backgroundColor = .systemOrange
+    }
+
+}
+
