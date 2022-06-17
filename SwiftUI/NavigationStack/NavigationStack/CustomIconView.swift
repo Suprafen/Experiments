8ba34e968e97893154721791ef9afdce21881c48
Ivan <ivan.skif41@gmail.com>
2022-06-17 14:07:01 +0300

    Add custom item view with init

---
+++ SwiftUI/NavigationStack/NavigationStack/CustomIconView.swift
@@ -0,0 +1,37 @@
+//
+//  CustomIconView.swift
+//  NavigationStack
+//
+//  Created by Ivan Pryhara on 17.06.22.
+//
+
+import SwiftUI
+
+struct CustomIconView: View {
+    var color: Color
+    var systemNameImageStr: String
+    var description: String
+    
+    init(color: Color = .blue, systemNameImageStr: String = "externaldrive.fill", description: String = "Drive") {
+        self.color = color
+        self.systemNameImageStr = systemNameImageStr
+        self.description = description
+    }
+    
+    var body: some View {
+        VStack {
+            Image(systemName: systemNameImageStr)
+            Text(description)
+        }
+        .background(in: Circle().inset(by: -20))
+        .backgroundStyle(color.gradient)
+        .foregroundStyle(.white.shadow(.drop(radius: 1, y: 1.5)))
+        .padding(20)
+    }
+}
+
+struct CustomIconView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomIconView()
+    }
+}
