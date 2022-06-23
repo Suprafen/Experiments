b924c8c0fc725b953b2db6c25bbf534a1ba5e7cf
Ivan <ivan.skif41@gmail.com>
2022-06-23 08:38:50 +0300

    Add new views and folders for testing Navigation Stack

---
+++ SwiftUI/NavigationStack/NavigationStack/Views/AddButtonView.swift
@@ -0,0 +1,31 @@
+//
+//  AddButtonView.swift
+//  NavigationStack
+//
+//  Created by Ivan Pryhara on 20.06.22.
+//
+
+import SwiftUI
+
+struct AddButtonView: View {
+    @State var isSheetShown: Bool = false
+    
+    var body: some View {
+        Button {
+            isSheetShown = true
+        } label: {
+            HStack {
+                Image(systemName: "plus")
+                Text("Add")
+            }
+        }.sheet(isPresented: $isSheetShown) {
+            CustomIconView()
+        }
+    }
+}
+
+struct AddButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddButtonView()
+    }
+}
