094156e703336bc85a107542e7d3bfada5563200
Ivan <ivan.skif41@gmail.com>
2022-07-20 14:31:01 +0300

    Continuation test

---
+++ SwiftUI/Concurrency/Concurrency/Continuation.swift
@@ -0,0 +1,75 @@
+//
+//  Continuation.swift
+//  Concurrency
+//
+//  Created by Ivan Pryhara on 18.07.22.
+//
+
+import SwiftUI
+
+
+struct Continuation: View {
+    
+    struct Message: Decodable, Identifiable {
+        let id: Int
+        let from: String
+        let message: String
+    }
+
+    @State var messages: [Message] = []
+    
+    var body: some View {
+        List(messages, id: \.self.id) { message in
+            VStack {
+                HStack {
+                    Text("From: ").font(.headline)
+                    Text(message.from)
+                    Spacer()
+                }
+                Spacer()
+                HStack {
+                    Text(message.message).font(.callout)
+                    Spacer()
+                }
+            }
+        }
+        .onAppear {
+            Task {
+                messages = await fetchMessages()
+            }
+        }
+        .refreshable {
+            Task {
+                messages = await fetchMessages()
+            }
+        }
+    }
+    
+    func fetchMessages(completion: @escaping ([Message]) -> Void) {
+        let url = URL(string: "https://hws.dev/user-messages.json")!
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                if let messages = try? JSONDecoder().decode([Message].self, from: data) {
+                    completion(messages)
+                    return
+                }
+            }
+            completion([])
+        }.resume()
+    }
+    
+    func fetchMessages() async -> [Message] {
+        await withCheckedContinuation{ continuation in
+            fetchMessages { messages in
+                continuation.resume(returning: messages)
+            }
+        }
+    }
+}
+
+struct Continuation_Previews: PreviewProvider {
+    static var previews: some View {
+        Continuation()
+    }
+}
