d98016221d8da68fb5e041136c6fd70e42066a13
Ivan <ivan.skif41@gmail.com>
2022-09-07 12:22:49 +0200

    Initial commit

---
+++ SwiftUI/WatchOS/WatchOS-Playground/WatchOS-Playground Watch App/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WatchOS-Playground Watch App
+//
+//  Created by Ivan Pryhara on 7.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/WatchOS/WatchOS-Playground/WatchOS-Playground Watch App/WatchOS_PlaygroundApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchOS_PlaygroundApp.swift
+//  WatchOS-Playground Watch App
+//
+//  Created by Ivan Pryhara on 7.09.22.
+//
+
+import SwiftUI
+
+@main
+struct WatchOS_Playground_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                ContentView()
+            }
+        }
+    }
+}
