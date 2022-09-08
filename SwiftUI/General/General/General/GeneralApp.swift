9ecb39ed90e3462225feb43843cbb421b93097f8
Ivan <ivan.skif41@gmail.com>
2022-09-08 12:29:05 +0200

    initial commit

---
+++ SwiftUI/General/General/General/GeneralApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeneralApp.swift
+//  General
+//
+//  Created by Ivan Pryhara on 8.09.22.
+//
+
+import SwiftUI
+
+@main
+struct GeneralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
