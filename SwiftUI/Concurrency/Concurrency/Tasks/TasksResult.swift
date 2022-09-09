1de7889cbaa3e05e1721584626348adbc9eb43a2
Ivan <ivan.skif41@gmail.com>
2022-09-09 17:50:13 +0200

    Try different ways to get data from the Task

--- SwiftUI/Concurrency/Concurrency/Tasks/TasksResult.swift
+++ SwiftUI/Concurrency/Concurrency/Tasks/TasksResult.swift
@@ -13,53 +13,94 @@ enum LoadError: Error {
     case fetchFailed, decodeFailed
 }
 
-func fetchQuotes(completion: @escaping (String) -> ()) async {
-    let downloadTask = Task { () -> String in
-        let url = URL(string: "https://hws.dev/quotes.txt")!
-        let data: Data
-        
-        do {
-            (data, _) = try await URLSession.shared.data(from: url)
-        } catch {
-            throw LoadError.fetchFailed
-        }
-        
-        if let string = String(data: data, encoding: .utf8) {
-            return string
-        } else {
-            throw LoadError.decodeFailed
-        }
-    }
+struct Post: Codable, Identifiable, Hashable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
     
-    let result  = await downloadTask.result
-    
-    do {
-        let string = try result.get()
-        completion(string)
-        print(string)
-        
-    } catch LoadError.decodeFailed {
-        completion("Unable to convert quotes to text.")
-        
-        print("Unable to convert quotes to text.")
-    } catch LoadError.fetchFailed {
-        completion("Unable to fetch the quotes.")
-        
-        print("Unable to fetch the quotes.")
-    } catch {
-        completion("Unable to fetch the quotes.")
-        
-        print("Unknown error.")
+    enum CodingKeys: String, CodingKey {
+        case userId = "userId"
+        case id = "id"
+        case title = "title"
+        case body = "body"
     }
 }
 
 struct TasksResult: View {
     @State var quotes: String = ""
+    @State var posts: [Post] = []
+    @State var isLoading: Bool = false
     var body: some View {
-        NavigationStack {
-            VStack {
-                Text("")
+        if isLoading {
+            ProgressView()
+        } else {
+            NavigationStack {
+                List(posts) { post in
+                    NavigationLink(value: post) {
+                        VStack {
+                            HStack {
+                                Text(post.title)
+                                Text("\(post.userId)")
+                            }
+                        }
+                    }
+                }
+                .listStyle(.plain)
+                .navigationDestination(for: Post.self) { post in
+                    List {
+                        Text(post.body)
+                    }
+                }
+            }
+            .onAppear {
+                isLoading = posts.isEmpty
+                Task {
+                    await fetchPosts()
+                    isLoading = false
+                }
             }
         }
+    }
+    // MARK: Task's result
+    func fetchQuotes() async {
+        let fetchTask = Task { () -> [Post] in
+            let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+            
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                return try JSONDecoder().decode([Post].self, from: data)
+            } catch {
+                return [Post(userId: 0, id: 0, title: "Error", body: "Error")]
+            }
+        }
+        
+        let taskResult = await fetchTask.result
+        
+        do {
+            let posts = try taskResult.get()
+            
+            self.posts = posts
+        } catch {
+            self.posts = [Post(userId: 0, id: 0, title: "Error", body: "Error")]
+        }
+    }
+    
+    // MARK: Task's value
+    func fetchPosts() async {
+        let fetchTask = Task { () -> [Post] in
+            let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+            
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                return try JSONDecoder().decode([Post].self, from: data)
+            } catch {
+                return [Post(userId: 0, id: 0, title: "Something Wrong happened", body: "Something Wrong happened")]
+            }
+        }
+        
+        let posts = await fetchTask.value
+        
+        self.posts = posts
     }
 }
