354202527624fa764199e0bb7597f1fe57d38325
Ivan <ivan.skif41@gmail.com>
2023-02-17 11:29:16 +0100

    Chnage base map SDK to MapKit

---
+++ SwiftUI/MapProject/MapProject/MapProject/App+MapKit/App/App+MapKit.swift
@@ -0,0 +1,18 @@
+//
+//  App+MapKit.swift
+//  MapProject
+//
+//  Created by Ivan Pryhara on 17.02.23.
+//
+
+import SwiftUI
+
+@main
+struct MapProjectApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            BaseView()
+        }
+    }
+}
