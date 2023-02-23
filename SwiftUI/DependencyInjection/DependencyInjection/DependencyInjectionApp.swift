9e41b54b049b0da9832036953818f73c613f47a4
Ivan <ivan.skif41@gmail.com>
2023-02-23 15:25:56 +0100

    DI project initial commit

---
+++ SwiftUI/DependencyInjection/DependencyInjection/DependencyInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionApp.swift
+//  DependencyInjection
+//
+//  Created by Ivan Pryhara on 23.02.23.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
