73cc3f715e27f7ab39bca027d07336e1316dc036
Ivan <ivan.skif41@gmail.com>
2023-03-16 17:31:59 +0100

    make small changes in StoryView

--- SwiftUI/Sport-Crowd/Sport-Crowd/submodules/HomeView/StoryView.swift
+++ SwiftUI/Sport-Crowd/Sport-Crowd/submodules/HomeView/StoryView.swift
@@ -35,10 +35,37 @@ struct StoryView: View {
                     .opacity(0.1)
             }
             .frame(width: width * 0.95, height: height * 0.95)
-            Circle()
+            CircleShape(startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
                 .strokeBorder(.red, style: StrokeStyle(lineWidth: width * 0.05, lineCap: .round, lineJoin: .round))
                 .frame(width: width, height: height)
                 .opacity(storyWatched ? 0 : 1)
         }
+    }
+}
+
+private struct CircleShape: InsettableShape {
+    
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insetAmount = 0.0
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        
+        return path
+        
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
     }
 }
